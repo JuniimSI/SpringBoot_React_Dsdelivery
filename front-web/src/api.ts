ebcdda2a8e5517ec2de578e011fa8eb8b85afbe7
Francisco Fernandes Júnior <jrfernandesti@gmail.com>
2021-01-08 21:30:47 -0300

    Lasts COmmiteds

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
 
-const API_URL = 'https://junior-semana-dev-superior.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 export function fetchProducts () {
     return axios(`${API_URL}/products`);
